6f7a2085f60efe29c6e3f5869eaa437cb7cdfef1
jun <jun@funble.kr>
2025-02-21 20:51:12 +0900

    Initial Commit

---
+++ src/main/kotlin/com/rspt/announcement/AnnouncementApplication.kt
@@ -0,0 +1,11 @@
+package com.rspt.announcement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnnouncementApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnnouncementApplication>(*args)
+}
---
+++ src/test/kotlin/com/rspt/announcement/AnnouncementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rspt.announcement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnnouncementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

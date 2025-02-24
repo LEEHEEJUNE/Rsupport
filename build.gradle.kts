9a91c2dfe83a679ca0ce6dfa322dfa1d739c9ac2
jun <jun@funble.kr>
2025-02-24 23:15:41 +0900

    feat: gradle 세팅, application.yml 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
+
 group = "com.rspt"
 version = "0.0.1-SNAPSHOT"
 
@@ -13,20 +14,34 @@ java {
 	}
 }
 
-
 repositories {
 	mavenCentral()
+	maven { url = uri("https://jitpack.io") }
 }
-
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+
+	implementation("org.hibernate:hibernate-envers:6.4.2.Final")
+
+	implementation("com.google.code.gson:gson:2.10.1")
+
+	implementation("org.slf4j:slf4j-api:2.0.9")
+	implementation("ch.qos.logback:logback-classic:1.5.16")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.22")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// MySQL Connector/J (DB 연결)
+	runtimeOnly("mysql:mysql-connector-java:8.0.33")
 }
 
 kotlin {
@@ -41,4 +56,3 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 		jvmTarget = "21"
 	}
 }
-
